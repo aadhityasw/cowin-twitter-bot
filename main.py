c662a5eadd3e0f68eda300d1bc9787b83d661a81
aadhityasw <aadhityas@gmail.com>
2021-06-08 23:08:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def pingCovinForInfo() :
     pings_left = 5
     while pings_left > 0 :
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         }
         parameters = {
             'district_id' : "540",
@@ -69,6 +69,7 @@ def pingCovinForInfo() :
     if 200 <= response.status_code < 300 :
         return response.json()
     else :
+        print(response.status_code)
         print(response.json())
     return None
 
