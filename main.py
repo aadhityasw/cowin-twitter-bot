2cfcfea4fc3fb11eda3540abb8dfc7a2cb1c3f5c
aadhityasw <aadhityas@gmail.com>
2021-06-08 23:20:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ def pingCovinForInfo() :
     pings_left = 5
     while pings_left > 0 :
         headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+            #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
         }
         parameters = {
             'district_id' : "540",
